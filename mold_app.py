64a9209187b663d5f99dbc6ad10f617b1dae8a87
woukl22 <woukl22@gmail.com>
2024-10-31 01:20:36 +0900

    Feat: Move data selection to sidebar

--- mold_app.py
+++ mold_app.py
@@ -4,12 +4,11 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv('./data.csv', encoding='cp949', low_memory=False)
-
 df = df.drop(columns=['Unnamed: 0', 'line', 'name', 'mold_name', 'emergency_stop'])
 df = df.drop(index=19327)
 df = df.rename(columns={'date': 'temp_date', 'time': 'date'}).rename(columns={'temp_date': 'time'})
 
-# 결측치 처리
+# 결측치 제거
 molten_filtered = df['molten_volume'].loc[~df['molten_volume'].isin([0, 2767])].dropna()
 molten_median = molten_filtered.median()
 df['molten_volume'] = df['molten_volume'].fillna(molten_median)
@@ -20,27 +19,20 @@ df['tryshot_signal'] = df['tryshot_signal'].map({'D': 1}).fillna(0)
 df['heating_furnace'] = df['heating_furnace'].map({'A': 1, 'B': 2}).fillna(0)
 df['datetime'] = pd.to_datetime(df['date'] + ' ' + df['time'], errors='coerce')
 
-# mold code
-mold_codes = {
-    '8917': df[df['mold_code'] == 8917],
-    '8412': df[df['mold_code'] == 8412],
-    '8722': df[df['mold_code'] == 8722],
-    '8573': df[df['mold_code'] == 8573],
-    '8413': df[df['mold_code'] == 8413],
-    '8600': df[df['mold_code'] == 8600],
-    '8576': df[df['mold_code'] == 8576]
-}
+mold_codes = {f"{code} ({len(df[df['mold_code'] == code])})": df[df['mold_code'] == code]
+              for code in df['mold_code'].unique()}
 
-# streamlit
-st.title("Dynamic Data Plotting")
-adf_key = st.selectbox("Choose mold_code:", options=mold_codes.keys())
+st.sidebar.title("Data Selection")
+adf_key = st.sidebar.selectbox("Mold Code:", options=list(mold_codes.keys()))
 adf = mold_codes[adf_key]
 
-column_data = st.selectbox("Select column to plot:", options=adf.columns)
-start_date = st.date_input("Start Date:", value=pd.to_datetime("2019-01-01"))
-end_date = st.date_input("End Date:", value=pd.to_datetime("2019-04-01"))
+column_data = st.sidebar.selectbox("Column:", options=adf.columns)
+start_date = st.sidebar.date_input("Start Date:", value=pd.to_datetime("2019-01-01"))
+end_date = st.sidebar.date_input("End Date:", value=pd.to_datetime("2019-04-01"))
 
 filtered_data = adf[(adf['datetime'] >= pd.to_datetime(start_date)) & (adf['datetime'] <= pd.to_datetime(end_date))]
+
+st.title("Mold Data Visualization")
 
 if not filtered_data.empty:
     filtered_data['date'] = pd.to_datetime(filtered_data['date'])
